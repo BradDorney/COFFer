3d84629f4872c12a46f520e291640aada5d2f347
BradDorney <brad.dorney@gmail.com>
2017-02-08 19:39:02 -0500

    Added unmodified version disclaimer

--- ReaderCOFF (original).cpp
+++ ReaderCOFF (original).cpp
@@ -5,6 +5,9 @@
 // This file is distributed under the University of Illinois Open Source
 // License. See LICENSE.TXT for details.
 //
+// ** This is the original, unmodified version of ReaderCOFF.cpp from LLD. It is
+// provided here purely for comparison the modified version. **
+//
 //===----------------------------------------------------------------------===//
 
 #include "Atoms.h"
